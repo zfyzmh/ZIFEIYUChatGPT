c97401a58e8eeb87dc1d20d7d34df7454e7e6261
周明辉 <2693378384@qq.com>
2023-03-06 15:35:46 +0800

    添加数据库访问示例

--- ZIFEIYU/DataBase/ZFYDatabase.cs
+++ ZIFEIYU/DataBase/ZFYDatabase.cs
@@ -20,9 +20,45 @@ namespace ZIFEIYU.DataBase
         {
         }
 
-        public void Init()
-        {
-            CreateTableResult result = Database.CreateTableAsync<Test>().Result;
-        }
+        //public void Init()
+        //{
+        //    CreateTableResult result = Database.CreateTableAsync<Test>().Result;
+        //}
+
+        //public async Task<List<TodoItem>> GetItemsAsync()
+        //{
+        //    await Init();
+        //    return await Database.Table<TodoItem>().ToListAsync();
+        //}
+
+        //public async Task<List<TodoItem>> GetItemsNotDoneAsync()
+        //{
+        //    await Init();
+        //    return await Database.Table<TodoItem>().Where(t => t.Done).ToListAsync();
+
+        //    // SQL queries are also possible
+        //    //return await Database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        //}
+
+        //public async Task<TodoItem> GetItemAsync(int id)
+        //{
+        //    await Init();
+        //    return await Database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        //}
+
+        //public async Task<int> SaveItemAsync(TodoItem item)
+        //{
+        //    await Init();
+        //    if (item.ID != 0)
+        //        return await Database.UpdateAsync(item);
+        //    else
+        //        return await Database.InsertAsync(item);
+        //}
+
+        //public async Task<int> DeleteItemAsync(TodoItem item)
+        //{
+        //    await Init();
+        //    return await Database.DeleteAsync(item);
+        //}
     }
 }
