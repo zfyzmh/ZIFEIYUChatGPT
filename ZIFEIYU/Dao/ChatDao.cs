147291be0b3bf8b1d6ceebbe29c7dd30e3660e45
周明辉 <2693378384@qq.com>
2023-03-21 14:01:45 +0800

    历史对话功能准备工作

--- ZIFEIYU/Dao/ChatDao.cs
+++ ZIFEIYU/Dao/ChatDao.cs
@@ -1,6 +1,7 @@
 ﻿using SQLite;
 using ZIFEIYU.DataBase;
 using ZIFEIYU.Entity;
+using System.Linq;
 
 namespace ZIFEIYU.Dao
 {
@@ -16,6 +17,17 @@ namespace ZIFEIYU.Dao
         public async Task<List<ChatEntity>> GetAllChat()
         {
             return await _database.Table<ChatEntity>().ToListAsync();
+        }
+
+        /// <summary>
+        /// 获取对话列表倒序
+        /// </summary>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
+        public async Task<List<ChatEntity>> GetChatListDesc(int pageSize, int pageNumber)
+        {
+            return await _database.Table<ChatEntity>().OrderByDescending(s => s.Id).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
         }
 
         public async Task<ChatEntity> GetChatById(int id)
@@ -40,10 +52,26 @@ namespace ZIFEIYU.Dao
             }
         }
 
+        /// <summary>
+        /// 获取当前聊天
+        /// </summary>
+        /// <returns></returns>
         public async Task<ChatEntity> GetChatCurrent()
         {
             var count = await _database.Table<ChatEntity>().CountAsync();
             return await _database.Table<ChatEntity>().Skip(count - 1).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// 切换聊天
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ChatEntity> SwitchChat(ChatEntity chat)
+        {
+            var count = await _database.Table<ChatEntity>().DeleteAsync(e => e.Id == chat.Id);
+            chat.Id = 0;
+            await SaveChat(chat);
+            return chat;
         }
     }
 }
---
+++ ZIFEIYU/Global/LinqExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Linq
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<TSource> Page<TSource>(this IEnumerable<TSource> source, int pageSize, int pageNumber)
+        {
+            return source.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+    }
+}
